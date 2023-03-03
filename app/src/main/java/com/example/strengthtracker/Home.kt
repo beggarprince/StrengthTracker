537116278596e838097a5bbb6627c83e7dfeb92e
Pedro <palvarez122@hotmail.com>
2023-03-03 17:25:13 -0600

    vm

--- app/src/main/java/com/example/strengthtracker/Home.kt
+++ app/src/main/java/com/example/strengthtracker/Home.kt
@@ -1,41 +1,37 @@
 package com.example.strengthtracker
 
-import android.content.Context
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_home.*
-import android.widget.Toast
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.gson.Gson
-import java.io.FileOutputStream
-import com.google.firebase.storage.ktx.storage
+import kotlinx.android.synthetic.main.activity_home.*
 import java.io.File
+import java.io.FileOutputStream
+
 
 
 class Home : AppCompatActivity(), LifecycleOwner
 {
 private lateinit var cardAdapter: CardAdapter
-    
+lateinit var vm: HomeViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         //Setup Firebase Storage
+        val firebaseUser = FirebaseAuth.getInstance().currentUser
         val storage = FirebaseStorage.getInstance().reference
         val cardStorage = storage.child("csv/cardList.txt")
-
-        // Context.getFilesDir().getPath()
-        /*Path to the cardListCSV
-        /data/data/com.example.strengthtracker/files/cardList.csv
-        */
 
         //Setup RecyclerView
         cardAdapter = CardAdapter(mutableListOf())
@@ -49,14 +45,15 @@ private lateinit var cardAdapter: CardAdapter
 
         //File setup
         val fileOutputStream: FileOutputStream = openFileOutput("cardList.csv", MODE_APPEND)
-        var cardListCsvFile = Uri.fromFile(File("/data/data/com.example.strengthtracker/files/cardList.csv"))
+        var cardListCsvFile = Uri.fromFile(File(
+            "csv/"+ firebaseUser + "/cardList.csv"))
+        //var userCsvReference = storage.child("users/"+ currentFirebaseUser + "/cardList.csv")
 
         //Toast messages
         val emptyField = "Please fill out field(s)"
         val duration = Toast.LENGTH_SHORT
         val emptyFieldCardCreation = Toast.makeText(applicationContext, emptyField, duration)
 
-        //Variables
         var data: String
         var gson = Gson()
         var json: String
@@ -80,7 +77,8 @@ private lateinit var cardAdapter: CardAdapter
                 fileOutputStream.write(data.toByteArray())
                // json = gson.toJson(card)
                 //cardStorage.putBytes(data.toByteArray())
-                cardStorage.putFile(cardListCsvFile)
+                //cardStorage.putFile(cardListCsvFile)
+
                 newCard.visibility = View.INVISIBLE
             }
             else{
---
+++ app/src/main/java/com/example/strengthtracker/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.strengthtracker
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel(){
+
+
+}
