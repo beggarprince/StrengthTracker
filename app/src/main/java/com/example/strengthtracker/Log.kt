c0d8d7741e050ca1120c7dde876726ce829b03ac
Pedro <palvarez122@hotmail.com>
2023-03-13 21:42:00 -0500

    Kotlin pre-migration, small changes to log xml/kt

--- app/src/main/java/com/example/strengthtracker/Log.kt
+++ app/src/main/java/com/example/strengthtracker/Log.kt
@@ -2,14 +2,23 @@ package com.example.strengthtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class Log : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
+
         val title = findViewById<TextView>(R.id.logTitle)
+        val days = findViewById<TextView>(R.id.dayRecorded)
+        val recordLog = findViewById<Button>(R.id.newLog)
+
         title.text = intent.extras?.getString("Card")
+
+        recordLog.setOnClickListener{
+
+        }
 
     }
 }
